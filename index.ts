2e0dec92eb6360f02fddbb86bd44fbf8567d57b5
Richard Jedlička <jedlicka.r@gmail.com>
2022-03-06 17:14:06 +0100

    scrape and download city flag

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
@@ -23,6 +25,38 @@ export class CapitalCityScraper {
 		const populationText = populationRows.find('th:contains(Capital city) + td').text().trim();
 		const population = parseFloat(populationText.replace(/,/g, ''));
 		console.log(population);
+
+		const flagPageLink = $('.mergedtoprow a.image + div:contains(Flag)').prev().attr('href')!;
+		const flagPageUrl = new URL(flagPageLink, url).toString();
+		const flagImagePath = await this.scrapeImage(flagPageUrl);
+		console.log(flagImagePath);
+	}
+
+	protected async scrapeImage(url: string) {
+		const response = await axios.get(url);
+		const html = response.data;
+
+		const doc = cheerio.load(html);
+
+		const imageLink = doc('#file a').attr('href')!;
+		const imageUrl = new URL(imageLink, url).toString();
+
+		const imagePath = await this.downloadFile(imageUrl, 'flags');
+
+		return imagePath;
+	}
+
+	protected async downloadFile(url: string, dir: string) {
+		const response = await axios.get(url, {
+			responseType: 'arraybuffer'
+		});
+
+		fs.mkdirSync(dir, { recursive: true });
+
+		const filePath = path.join(dir, path.basename(url));
+		fs.writeFileSync(filePath, response.data);
+
+		return filePath;
 	}
 }
 
