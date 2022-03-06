f13ccee0113d3da1f3ba4542c135d587d0f95c5f
Richard Jedlička <jedlicka.r@gmail.com>
2022-03-06 16:54:04 +0100

    scrape city name

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Web scraping with Node.js and Typescript
 
 Source code for article series [Web scraping with Node.js and Typescript](https://dev.to/uiii/series/16972)
+
+## How to run
+
+```
+npm install
+npx ts-node index.ts
+```
---
+++ index.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+export class CapitalCityScraper {
+	async scrapeCity(url: string) {
+		const response = await axios.get(url);
+		const html = response.data;
+
+		const $ = cheerio.load(html);
+
+		const cityName = $('#firstHeading').text().trim();
+		console.log(cityName);
+	}
+}
+
+async function main() {
+	const scraper = new CapitalCityScraper();
+	await scraper.scrapeCity("https://en.wikipedia.org/wiki/Prague");
+}
+
+main();
